5fed98db7b187250a3386fbf4e085d89fdf479e6
RamCreator <rammaye87540644@gmail.com>
2023-12-24 19:08:49 +0530

    all_changes

---
+++ first.js
@@ -0,0 +1,127 @@
+// let js ='amazing';
+// if (js === 'amazing') alert('ram is good boy');
+
+// 48 + 8 + 23 - 10;
+// console.log(48 + 8 + 23 -10);
+
+// function square(y) {
+//     let x = 5; 
+//     let r = x ** y; 
+//     return r;
+// }
+
+// let result = square(3);
+// console.log(result);
+
+
+// let javascriptfun = true
+// console.log(javascriptfun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptfun);
+// console.log(typeof 23);
+// console.log(typeof "ram");
+
+// let department ;
+// console.log(department);
+// console.log(typeof department);
+
+// let scarry = 3.5788;
+// console.log(scarry);
+// console.log(typeof scarry);
+
+// let ES2020 = 8364375788;
+// console.log(ES2020);
+// console.log(typeof ES2020);
+
+// console.log(typeof null);
+// console.log(typeof ES2020);
+// x = 10;
+// y = 5;
+// z = x + y
+// console.log(z);
+
+// z = x * y
+// console.log(z);
+
+// z = x ** y
+// console.log(z);
+
+// z = x / y
+// console.log(z);
+
+// z = x % y
+// console.log(z);
+
+// z = x - y
+// console.log(z);
+
+// x = 35
+// console.log(x);
+
+// x +=  1;
+// console.log(x);
+
+// x -= 1 ;
+// console.log(x);
+
+
+// a =3;
+// b=9; 
+// console.log(a==b); 
+// console.log(a===b); 
+// console.log(a>b); 
+// console.log(a<b); 
+// console.log(a>=b); 
+// console.log(a<=b); 
+// console.log(a!=b); 
+// console.log(a!==b); 
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn=1.95;
+
+// const  BMIMark = massMark /heightMark ** 2;
+// const  BMIjohn = massJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIjohn;
+
+// console.log(BMIMark,BMIjohn,markHigherBMI);
+
+// const now = 2037;
+// const ram = now - 1991;
+// const kottesh = 2018;
+// console.log(now-1991>now-2018);
+
+// let x,y;
+// x = y=25-13-4;
+// console.log(x,y)
+// const averageAge =( ram+kottesh)/2
+// console.log(ram,kottesh,averageAge)
+
+
+
+
+
+const ramWeight = 78; // Ram weight 
+const ramHeight = 1.69; // Ram height 
+
+
+const sakthiWeight = 92; // Sakthi's weight 
+const sakthiHeight = 1.95; // Sakthi's height 
+// Calculate Ram  the BMI formula
+const ramBMI = ramWeight / (ramHeight ** 2);
+
+// Calculate Sakthi BMI formula
+const sakthiBMI = sakthiWeight / (sakthiHeight ** 2);
+
+//  Ram's BMI to Sakthi's BMI and store the result in a variable
+const ramHigherBMI = ramBMI > sakthiBMI;
+
+
+console.log("Ram BMI: " + ramBMI);
+console.log("Sakthi BMI: " + sakthiBMI);
+console.log("Ram has a higher BMI than Sakthi? " + ramHigherBMI);   
+
+
+
